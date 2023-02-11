318e01e6e47245fd5677a32777177922dfd34085
Ryo <tzl19961216@gmail.com>
2023-02-11 18:43:49 +0800

    feat: remove viewer request for cloudfront

--- website/website.ts
+++ website/website.ts
@@ -190,11 +190,6 @@ export class WebSite extends pulumi.ComponentResource {
 
           lambdaFunctionAssociations: [
             {
-              eventType: "viewer-request",
-              lambdaArn: args.viewerRequestLambdaArn,
-              includeBody: false,
-            },
-            {
               eventType: "origin-request",
               lambdaArn: args.originRequestLambdaArn,
               includeBody: false,
