4c518ebd9b71ce866dca4571e69576bc1e4678b4
Ryo <tzl19961216@gmail.com>
2023-02-11 18:25:40 +0800

    feat: allow multiple host for lambda edge

--- lambda/lambdas/nextBlogOriginRequest/index.js
+++ lambda/lambdas/nextBlogOriginRequest/index.js
@@ -3,7 +3,7 @@
 const path = require("path");
 exports.handler = async (event) => {
   const request = event.Records[0].cf.request;
-  const domain = "test.ryo-okami.xyz";
+  const host = request.headers.host[0].value;
   // Redirect trailing slashes
   if (request.uri !== "/" && request.uri.slice(-1) === "/") {
     return {
@@ -13,13 +13,13 @@ exports.handler = async (event) => {
         location: [
           {
             key: "Location",
-            value: `https://${domain}${request.uri.slice(0, -1)}`,
+            value: `https://${host}${request.uri.slice(0, -1)}`,
           },
         ],
       },
     };
   }
-  // Rewrite clean URLs (adding index.html)
+  // Rewrite clean URLs (adding .html)
   if (!path.extname(request.uri)) {
     request.uri = request.uri.replace(/\/?$/, ".html");
   }
